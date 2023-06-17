96491a8e54230c9543766b7893ba80471e67d7cc
Yasin Munoz <yasinmunoz@hotmail.com>
2023-06-17 20:00:00 +0200

    google-auth

--- src/app/google-auth/services/google-auth.service.ts
+++ src/app/google-auth/services/google-auth.service.ts
@@ -44,8 +44,8 @@ export class GoogleAuthService {
 
   userProfileSubject = new Subject<UserInfo>();
 
-  serverURL = 'http://localhost:3000/';
-  //serverURL = 'http://3.69.12.0:3000/';
+  //serverURL = 'http://localhost:3000/';
+  serverURL = 'http://3.69.12.0:3000/';
   apiURL = 'api/users'
 
   constructor(
